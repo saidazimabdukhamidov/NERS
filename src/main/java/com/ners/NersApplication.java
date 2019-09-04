aea594647bfcf48afc61c46f743689ddfe191de8
Saidazim <saidazimabduhamidov@gmail.com>
2019-09-04 15:08:23 +0500

    starting NERS

---
+++ src/main/java/com/ners/NersApplication.java
@@ -0,0 +1,13 @@
+package com.ners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NersApplication.class, args);
+  }
+
+}
